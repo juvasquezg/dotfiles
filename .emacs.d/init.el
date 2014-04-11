adffb177a0ca5a258cdfdb746c06148764f490ea
juvasquezg <juvasquez88@gmail.com>
2014-04-10 23:34:13 -0500

    add auto-complete in emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -106,3 +106,19 @@
 ; try to automagically figure out indentation
 (setq py-smart-indentation t)
 
+; ------------------
+; -- Autocomplete --
+; ------------------
+
+(add-to-list 'load-path "~/.emacs.d/auto-complete-1.3.1")
+(require 'auto-complete)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-complete-1.3.1/dict")
+(require 'auto-complete-config)
+(ac-config-default)
+; Use dictionaries by default
+(setq-default ac-sources (add-to-list 'ac-sources 'ac-source-dictionary))
+(global-auto-complete-mode t)
+; Start auto-completion after 2 characters of a word
+(setq ac-auto-start 2)
+; case sensitivity is important when finding matches
+(setq ac-ignore-case nil)
